de43128c5a4c2202c15f0cd6a45bc8d0a2d58949
Fatou1412 <ndeyefatou2001@yahoo.fr>
2026-02-13 14:29:17 +0100

    Implémentation client et véhicule

---
+++ models/clients.py
@@ -0,0 +1,15 @@
+class client:
+    def __init__ (self,id_client, nom, prenom, mail, telephone,adresse):
+        self.id_client = id_client
+        self.nom = nom
+        self.prenom = prenom
+        self.mail = mail
+        self.telephone = telephone
+        self.adresse = adresse)
+        
+        def __str__ (self):
+            return f"client{self.id_client, self.nom, self.prenom, self.mail}"
+        
+        
+        
+        
---
+++ models/models/véhicules.py
@@ -0,0 +1,13 @@
+class Vehicule:
+    def __init__(self, id_vehicule, marque, modele, cylindree, kilometrage, date_mise_en_circulation):
+        self.id_vehicule = id_vehicule 
+        self.marque = marque 
+        self.modele = modele 
+        self.cylindree = int(cylindree) 
+        self.kilometrage = kilometrage 
+        self.date_mise_en_circulation = date_mise_en_circulation 
+
+    def __str__(self):
+        return f"{self.id_vehicule}\n{self.marque} {self.modele} ({self.cylindree} cyl., {self.kilometrage} km)" 
+
+    
